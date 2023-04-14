4fb27efe7671bc3cb0326845780d41f737237701
Soufiane LACHKAR <beca_is3@altertechnology.com>
2023-04-14 13:50:50 +0200

    metodo Paragraph

--- Main.py
+++ Main.py
@@ -1,16 +1,23 @@
 from abc import ABC, abstractmethod
 import pdfplumber
+import re
 
 
 class IOpen(ABC):
     @abstractmethod
     def open_file(path):
-        return path
+        pass
 
 class IRead(ABC):
     @abstractmethod
     def read_file(name, page):
         pass
+
+class IParagraph(ABC):
+    @abstractmethod
+    def get_paragraphs():
+        pass
+
 
 
 class OpenPDF(IOpen):
@@ -18,10 +25,29 @@ class OpenPDF(IOpen):
         file = pdfplumber.open(path)
         return file
 
-class ReadPdf(IRead):
+class ReadPDF(IRead):
     def read_file(file , page):
         content = file.pages[page -1].extract_text()
         return content
+
+class Paragraph(IParagraph):
+    def get_paragraphs(content, start_regex :re, end_regex :re):
+        paragraphs = []
+        paragraph = ""
+        current_title = ""
+        for line in content.split("\n"):
+            if(end_regex.match(line) and line != current_title and current_title != ""):
+                paragraphs.append(paragraph)
+                paragraph = ""
+                current_title = line
+            if(start_regex.match(line)):
+                paragraph += line + "\n"
+                current_title = line
+        return paragraphs
+            
+        
+                
+
 
 
 
@@ -31,4 +57,4 @@ file_name = "./files/esccrpqpl005iss235.pdf"
 
 file_read = OpenPDF.open(file_name)
 
-print(ReadPdf.read_file(file_read, 3))
+content = ReadPDF.read_file(file_read, 3)
