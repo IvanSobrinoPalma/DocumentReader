4bf05276f64691460e8d7b2cefb25ec43277208c
IVAN SOBRINO PALMA <beca_is2@altertechnology.com>
2023-04-17 11:18:46 +0200

    metodo filter en proceso

--- Main.py
+++ Main.py
@@ -13,11 +13,11 @@ file_read = OpenPDF.open(file_name)
 
 content = ReadPDFTable.read_file(file_read, 3)
 
-dictionary = {"Title": re.compile(r'[A-Z][a-zA-Z -]+:\s*'), "Certificate": re.compile(r'\d{2,3}[A-Z](?:[rev]{3}\d)?')}
+dictionary = {"Title": re.compile(r'[A-Z][a-zA-Z -]+:\s*'), "Certificate": re.compile(r'\d{2,3}[A-Z](?:[rev]{3}\d)?+,')}
 
 CERTIFICATE = re.compile(r'[A-Z][a-zA-Z -]+:\s*')
 CORREO = re.compile(r'Document:\s*')
 
 paragraphs = Paragraph.get_paragraphs(content, CERTIFICATE)
 
-Filter.filter_info(Paragraph.get_paragraphs(content, CERTIFICATE), dictionary)
+Filter.filter_info(paragraphs, dictionary)
--- service/Filter.py
+++ service/Filter.py
@@ -2,9 +2,15 @@ from interface.IFilter import IFilter
 
 class Filter(IFilter):
     def filter_info(paragraphs, dictionary):
+        result = {}
+        final_result = []
         for paragraph in paragraphs:
+            result = {}
             for line in paragraph.split("\n"):
                 for key in dictionary:
-                    key = dictionary[key].findall(line)
-                    print(key)
-        return dictionary
+                    data = dictionary[key].findall(line)
+                    if(len(data) > 0):
+                        result[key] = data
+            final_result.append(result)
+        print(final_result)
+        return final_result
