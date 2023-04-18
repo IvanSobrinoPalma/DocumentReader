49ed73bad019244ab703001f2046bba09809d4c6
Soufiane LACHKAR <beca_is3@altertechnology.com>
2023-04-18 09:36:05 +0200

    #

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from service.Paragraph import Paragraph
 from service.ReadPdfTable import ReadPDFTable
 from service.ReadPdfText import ReadPDFText
 from service.Filter import Filter
+from service.FilterDSCC import FilterDSCC
 
 
 
@@ -18,16 +19,21 @@ content = ReadPDFText.read_file(file_read, 1)
 dictionary = {"Title": re.compile(r'[A-Z][a-zA-Z -]+(?=:\s*)'), 
               "Certificate": re.compile(r'\d{2,3}[A-Z](?:[rev]{3}\d)?(?=, )'), 
               "Manufacturer": re.compile(r'[^ ,][a-zA-Z!@#$&()\\-`+\’ ]+\([a-zA-Z]+\)')}
-dictionary_2 = {"Document": re.compile(r'Document:\s*[a-zA-Z0-9!@#$&()\\-`+\/’, -]+'), 
-              "Description": re.compile(r'Description:\s*[a-zA-Z0-9!@#$&()\\-`+\/’, -]+')}
+dictionary_2 = {"Document": re.compile(r'^Document:\s*[a-zA-Z0-9!@#$&()\\-`+\/’, -]+'), 
+              "Description": re.compile(r'^Description:\s*[a-zA-Z0-9!@#$&()\\-`+\/’, -]+'),
+              "URL": re.compile(r'^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\/Downloads\/[a-zA-Z0-9-.? \/]+'),
+              "File Size": re.compile(r'^File size: [0-9 ]+ [kmghe]b'),
+              "More Info": re.compile(r'^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\/Programs\/[a-zA-Z0-9-.? \/]+')}
+titles = ["Document", "Description","URL","File size", "More Info"]
 
 CERTIFICATE = re.compile(r'[A-Z][a-zA-Z -]+:\s*')
 CORREO = re.compile(r'Document:\s*')
 
 paragraphs = Paragraph.get_paragraphs(content, CORREO)
 
-for line in Filter.filter_info(paragraphs, dictionary_2):
-    print(line)
-
+for paragraph in FilterDSCC.filter_data(paragraphs, titles):
+    for line in paragraph:
+        print(line + ": ")
+        print(paragraph[line])
 
 #Filter.filter_info(paragraphs, dictionary)
--- service/FilterDSCC.py
+++ service/FilterDSCC.py
@@ -1,27 +1,30 @@
 from interface.IFilter import IFilter
+import re
 
 class FilterDSCC(IFilter):
-    def filter_info(paragraphs, dictionary):
-        final_result = []
+    def filter_data(paragraphs, titles):
+        url_re = re.compile(r'^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\/')
+        result = []
         for paragraph in paragraphs:
-            result = {}
-            data = ""
+            data = {}
+            current_title = None
             for line in paragraph.split("\n"):
-                match_regex = False
-                for key in dictionary:
-                    current_regex = None
-                    if len(dictionary[key].findall(line)) > 0:
-                        match_regex = True 
-                        current_regex = dictionary[key]
-                        data = dictionary[key].findall(line)[0]
-                        if key in result:
-                            final_result.append(result)
-                            result = {}
-                        result[key] = data
-                            
-                if match_regex == False:
-                    data += line
-                    result[key] = data
+                match_title = False
+                for title in titles:
+                    if line.lower().startswith(title.lower()):
+                        # Quitar el titulo y las dos puntos
+                        text = line[len(title)+2:]
+                        current_title = title
+                        match_title = True
+                if not match_title:
+                    text += line
+                if current_title:
+                    # Comprobamos si el texto es un enlace, y lo quitamos los espacios blancos
+                    if url_re.match(text):
+                        text = text.replace(" ", "")
+                    data[current_title] = text
+                    
+            result.append(data)
+        return result
+                
 
-            final_result.append(result)
-        return final_result
